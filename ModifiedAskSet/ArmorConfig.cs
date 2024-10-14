58266771134cda3efec7a3556a91ad24410b97a2
Jumb0Frame <jumboframe90@gmail.com>
2024-10-14 14:27:44 +0200

    Armor Config

--- ModifiedAskSet/ArmorConfig.cs
+++ ModifiedAskSet/ArmorConfig.cs
@@ -11,32 +11,34 @@ namespace ModifiedAskSet
         private string name;
         private string description;
 
-        private int maxQuality;
+        private int maxQuality = 0;
 
-        private float weight;
+        private float weight = 0f;
 
-        private string setName;
-        private int setSize;
+        private StatusEffect equipStatusEffect = null;
 
-        private StatusEffect setStatusEffect;
+        private string setName = null;
+        private int setSize = 0;
+        private StatusEffect setStatusEffect = null;
 
-        private float movementModifier;
+        private float movementModifier = 0f;
 
-        private float armor;
-        private float armorPerLvl;
+        private float armor = 0f;
+        private float armorPerLvl = 0f;
 
-        private float maxDurability;
-        private float durabilityPerLvl;
+        private float maxDurability = 0f;
+        private float durabilityPerLvl = 0f;
 
-        private float equipTime;
+        private float equipTime = 0f;
 
 
-        public ArmorConfig(string _name, string _desc, int _maxQual, float _weight, string _setName, int _setSize, StatusEffect _setSE, float _movementMod, float _armor, float _armorPerLvl, float _maxDura, float _duraPerLvl, float _equipTime) 
+        public ArmorConfig(string _name, string _desc, int _maxQual, float _weight, StatusEffect _equipeSE, string _setName, int _setSize, StatusEffect _setSE, float _movementMod, float _armor, float _armorPerLvl, float _maxDura, float _duraPerLvl, float _equipTime) 
         { 
             name = _name;
             description = _desc;
             maxQuality = _maxQual;
             weight = _weight;
+            equipStatusEffect = _equipeSE;
             setName = _setName;
             setSize = _setSize;
             setStatusEffect = _setSE;
@@ -52,19 +54,18 @@ namespace ModifiedAskSet
         {
             item.m_itemData.m_shared.m_name = name;
             item.m_itemData.m_shared.m_description = description;
-            item.m_itemData.m_shared.m_maxQuality = maxQuality;
-            item.m_itemData.m_shared.m_weight = weight;
-            item.m_itemData.m_shared.m_setName = setName;
-            item.m_itemData.m_shared.m_setSize = setSize;
-            item.m_itemData.m_shared.m_setStatusEffect  = setStatusEffect;
-            item.m_itemData.m_shared.m_movementModifier = movementModifier;
-            item.m_itemData.m_shared.m_armor = armor;
-            item.m_itemData.m_shared.m_armorPerLevel = armorPerLvl;
-            item.m_itemData.m_shared.m_maxDurability = maxDurability;
-            item.m_itemData.m_shared.m_durabilityPerLevel = durabilityPerLvl;
-            item.m_itemData.m_shared.m_equipDuration = equipTime;
+            if (maxQuality > 0) item.m_itemData.m_shared.m_maxQuality = maxQuality;
+            if (weight > 0f) item.m_itemData.m_shared.m_weight = weight;
+            if (equipStatusEffect != null) item.m_itemData.m_shared.m_equipStatusEffect = equipStatusEffect;
+            if (setName != null) item.m_itemData.m_shared.m_setName = setName;
+            if (setSize > 0) item.m_itemData.m_shared.m_setSize = setSize;
+            if (setStatusEffect != null) item.m_itemData.m_shared.m_setStatusEffect  = setStatusEffect;
+            if (movementModifier != 0f) item.m_itemData.m_shared.m_movementModifier = movementModifier;
+            if (armor > 0f) item.m_itemData.m_shared.m_armor = armor;
+            if (armorPerLvl > 0f) item.m_itemData.m_shared.m_armorPerLevel = armorPerLvl;
+            if (maxDurability > 0f) item.m_itemData.m_shared.m_maxDurability = maxDurability;
+            if (durabilityPerLvl > 0f) item.m_itemData.m_shared.m_durabilityPerLevel = durabilityPerLvl;
+            if (equipTime != 0f) item.m_itemData.m_shared.m_equipDuration = equipTime;
         }
-
-        //todo anything else needed ?
     }
 }
