630380b7e294186dd24f1e7e641d9e7e3e5bef81
Jumb0Frame <jumboframe90@gmail.com>
2024-10-17 02:31:43 +0200

    Cleanup

--- ModifiedAskSet/ModifiedAskSet.cs
+++ ModifiedAskSet/ModifiedAskSet.cs
@@ -63,7 +63,7 @@ namespace ModifiedAskSet
             //get set effects
             SE_Stats setEffects = setStatusEffects.getSetEffects();
 
-            //todo : equip effects for each piece if any, recipe config for each, armor config for each
+            //todo : equip effects for each piece if anych
 
             //hood
             hoodRecipeConfig = new RecipeConfig("Better Ask Hood Config", "Better Ask Hood description config", "blackforge", "blackforge", 3, new RequirementConfig("MoltenCore", 2, 1),
@@ -72,27 +72,6 @@ namespace ModifiedAskSet
             CustomItem askHood = new CustomItem("ModifiedAskHood", "HelmetAshlandsMediumHood", hoodRecipeConfig.GetRecipeConfig());
             ItemManager.Instance.AddItem(askHood);
             hoodArmorConfig.ApplyConfig(askHood.ItemDrop);
-
-            /*
-            ItemConfig askHoodConfig = new ItemConfig();
-            askHoodConfig.Name = "Better Ask Hood Config";
-            askHoodConfig.Description = "Better Ask Hood description config";
-            askHoodConfig.CraftingStation = "blackforge";
-            askHoodConfig.AddRequirement(new RequirementConfig("MoltenCore", 2));
-            askHoodConfig.AddRequirement(new RequirementConfig("LinenThread", 15));
-            askHoodConfig.AddRequirement(new RequirementConfig("AskHide", 10));
-            askHoodConfig.AddRequirement(new RequirementConfig("LoxPelt", 4));
-
-            CustomItem askHood = new CustomItem("ModifiedAskHood", "HelmetAshlandsMediumHood", askHoodConfig);
-            ItemManager.Instance.AddItem(askHood);
-
-            var askHoodDrop = askHood.ItemDrop;
-            askHoodDrop.m_itemData.m_shared.m_name = "Better Ask Hood";
-            askHoodDrop.m_itemData.m_shared.m_description = "Better Ask Hood description";
-            askHoodDrop.m_itemData.m_shared.m_setName = "Modified Ask Set";
-            askHoodDrop.m_itemData.m_shared.m_setSize = 3;
-            askHoodDrop.m_itemData.m_shared.m_setStatusEffect = (StatusEffect)(object)SetEffects;
-            */
 
 
             //chest
@@ -103,27 +82,6 @@ namespace ModifiedAskSet
             ItemManager.Instance.AddItem(askChest);
             chestArmorConfig.ApplyConfig(askChest.ItemDrop);
 
-            /*
-            ItemConfig askChestConfig = new ItemConfig();
-            askChestConfig.Name = "Better Ask Chest Config";
-            askChestConfig.Description = "Better Ask Chest description config";
-            askChestConfig.CraftingStation = "blackforge";
-            askChestConfig.AddRequirement(new RequirementConfig("MoltenCore", 2));
-            askChestConfig.AddRequirement(new RequirementConfig("LinenThread", 15));
-            askChestConfig.AddRequirement(new RequirementConfig("AskHide", 10));
-            askChestConfig.AddRequirement(new RequirementConfig("LoxPelt", 4));
-
-            CustomItem askChest = new CustomItem("ModifiedAskChest", "ArmorAshlandsMediumChest", askChestConfig);
-            ItemManager.Instance.AddItem(askChest);
-
-            var askChestDrop = askChest.ItemDrop;
-            askChestDrop.m_itemData.m_shared.m_name = "Better Ask Chest";
-            askChestDrop.m_itemData.m_shared.m_description = "Better Ask Chest description";
-            askChestDrop.m_itemData.m_shared.m_setName = "Modified Ask Set";
-            askChestDrop.m_itemData.m_shared.m_setSize = 3;
-            askChestDrop.m_itemData.m_shared.m_setStatusEffect = (StatusEffect)(object)SetEffects;
-            */
-
             //legs
             legsRecipeConfig = new RecipeConfig("Better Ask Legs Config", "Better Ask Legs description config", "blackforge", "blackforge", 3, new RequirementConfig("MoltenCore", 2, 1),
                                                 new RequirementConfig("LinenThread", 15, 10), new RequirementConfig("AskHide", 10, 5), new RequirementConfig("LoxPelt", 4, 2));
@@ -132,55 +90,9 @@ namespace ModifiedAskSet
             ItemManager.Instance.AddItem(askLegs);
             legsArmorConfig.ApplyConfig(askLegs.ItemDrop);
 
-            /*
-            ItemConfig askLegsConfig = new ItemConfig();
-            askLegsConfig.Name = "Better Ask Legs Config";
-            askLegsConfig.Description = "Better Ask Legs description config";
-            askLegsConfig.CraftingStation = "blackforge";
-            askLegsConfig.AddRequirement(new RequirementConfig("MoltenCore", 2));
-            askLegsConfig.AddRequirement(new RequirementConfig("LinenThread", 15));
-            askLegsConfig.AddRequirement(new RequirementConfig("AskHide", 10));
-            askLegsConfig.AddRequirement(new RequirementConfig("LoxPelt", 4));
-
-            CustomItem askLegs = new CustomItem("ModifiedAskLegs", "ArmorAshlandsMediumlegs", askLegsConfig);
-            ItemManager.Instance.AddItem(askLegs);
-
-            var askLegsDrop = askLegs.ItemDrop;
-            askLegsDrop.m_itemData.m_shared.m_name = "Better Ask Legs";
-            askLegsDrop.m_itemData.m_shared.m_description = "Better Ask Legs description";
-            askLegsDrop.m_itemData.m_shared.m_setName = "Modified Ask Set";
-            askLegsDrop.m_itemData.m_shared.m_setSize = 3;
-            askLegsDrop.m_itemData.m_shared.m_setStatusEffect = (StatusEffect)(object)SetEffects;
-            */
-
             PrefabManager.OnVanillaPrefabsAvailable -= AddModifiedAsksvinArmor;
             
         }
-
-        /*
-        private void SetStatusEffects()
-        {
-            //todo
-
-            try
-            {
-                SetEffects = ScriptableObject.CreateInstance<SE_Stats>();
-                ((UnityEngine.Object)SetEffects).name = "Modified Ask Effect";
-                SetEffects.m_name = "Better Ask's Endurance";
-                SetEffects.m_tooltip = "Better Ask's tooltip";
-                SetEffects.m_percentigeDamageModifiers.m_slash = 0.15f;
-                SetEffects.m_percentigeDamageModifiers.m_pierce = 0.15f;
-                SetEffects.m_attackStaminaUseModifier = -0.2f;
-                SetEffects.m_runStaminaUseModifier = -0.1f;
-                SetEffects.m_jumpStaminaUseModifier = -0.1f;
-            }
-            catch (Exception e)
-            {
-                Jotunn.Logger.LogInfo(e);
-            }
-
-        }
-        */
     }
 }
 
